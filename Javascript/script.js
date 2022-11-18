b88d5fe20e13884cc65ec77583054cf06b7571ed
Harshvardhan Rathore <rharshvardhan614@gmail.com>
2022-11-18 19:17:30 +0500

    add scroll up button

--- Javascript/script.js
+++ Javascript/script.js
@@ -18,6 +18,22 @@ if(close){
     })
 }
 
+$(document).ready(function () {
+  $(window).scroll(function () {    
+    if (this.scrollY > 2000) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(" .scroll-up-btn").removeClass("show");
+    }
+  });
+});
+// slide up button 
+ //slide-up script
+ $('.scroll-up-btn').click(function(){
+  $('html').animate({scrollTop:0});
+});
+
+
 //technoloies part
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
